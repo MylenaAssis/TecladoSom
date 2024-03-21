ee29f9502031435b283273f5890d50414a358816
Mylena Assis <lena.assis.dev@gmail.com>
2024-03-21 15:52:45 -0300

    Fixed - uso do laco for

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ function tocaSom (idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    //console.log(idAudio);
-
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
-
-    contador = contador + 1;
-
-    //console.log(contador);
 }
