3c34a50e2d241cb025c006af625952b00c48ad6d
Mylena Assis <lena.assis.dev@gmail.com>
2024-03-24 18:40:26 -0300

    Fixed - teclas Space e Enter

--- main.js
+++ main.js
@@ -14,4 +14,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code ==='Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+    
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
+
